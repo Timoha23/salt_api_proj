92579fbd965332e5c23a00cca7dd5a5cf33e98eb
Tim <eqeq.tima@yandex.ru>
2024-06-27 12:39:39 +0500

    refact module

--- _modules/module_1.py
+++ _modules/module_1.py
@@ -1,5 +1,9 @@
 import random
 
 
-def start():
-    return random.randint(1, 100)
+def start(start, end):
+    if not (isinstance(start, int) and isinstance(end, int)):
+        raise Exception(
+            f'One or more args is not instnace int.\nStart: {start}, type: {type(start)}\nEnd: {end}, type: {type(end)}'
+        )
+    return random.randint(int(start), int(end))
